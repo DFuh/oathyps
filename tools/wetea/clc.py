57f6f8d614cc86994fbd035036a1ff67a99cc252
DFuh <dafu@uni-bremen.de>
2024-01-29 08:45:38 +0100

    create function for annuity-factor

--- tools/wetea/clc.py
+++ tools/wetea/clc.py
@@ -5,3 +5,22 @@ Created on Mon Jan 29 08:13:17 2024
 
 @author: DFuh
 """
+def clc_annuity_factor(interestrate, lifetime):
+    '''
+    Calculate the annuity factor
+
+    Parameters
+    ----------
+    interestrate : FLOAT
+        Theoretical interest rate for given investment in $\%/100$.
+    lifetime_we : INT
+        Lifetime of plant or system in years.
+
+    Returns
+    -------
+    annuity factor .
+
+    '''
+
+    return  (interestrate * (interestrate + 1)**lifetime ) /
+            ( ( ( interestrate + 1 )**lifetime ) - 1 )
