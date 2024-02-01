c631b90e8518139ecdc85a8cb3bb3e5373d7ae02
DFuh <dafu@uni-bremen.de>
2024-02-01 09:15:05 +0100

    misc

--- tools/wetea/clc.py
+++ tools/wetea/clc.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 29 08:13:17 2024
 
-@author: DFuh
+@author: David Fuhrländer
 """
 def clc_annuity_factor(interestrate, lifetime):
     '''
