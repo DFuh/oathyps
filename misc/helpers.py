4688675e0f28678ecb442045a19a58d7ba9755d7
DFuh <dafu@uni-bremen.de>
2024-02-01 09:39:25 +0100

    add helpers module

---
+++ misc/helpers.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Auxilliary functions.
+
+@author: David Fuhrländer
+"""
+
+import logging
+import datetime
+
+def timestamp(how='ISO'):
+    '''
+    Return datetimestamp as string
+        Default is "ISO", referring to ISO 8601.
+    '''
+
+    d = {'ym':'%y%m',
+         'ymd':'%y%m%d',
+         'yymd':'%Y%m%d',
+         'ISO':'%Y-%m-%d',
+         'ISO+md':'%Y-%m-%d_%H%M',
+         'yymdhm':'%Y%m%d%H%M'}
+
+    now = datetime.datetime.now()
+    strng = now.strftime(d.get(how,'ym'))
+
+    return strng
+
+
+
+def ini_logging(obj, name=None, pth='', level='INFO'):
+    '''
+    Initialize logging for console output only
+
+    '''
+    loggername = 'lggr_'+name
+    logger = logging.getLogger(loggername)
+
+
+    if level.upper() == 'DEBUG':
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    # remove all default handlers
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+
+    # create console handler and set level to debug
+    console_handle = logging.StreamHandler()
+    if level.upper() == 'DEBUG':
+        console_handle.setLevel(logging.DEBUG)
+    else:
+        console_handle.setLevel(logging.INFO)
+
+    # create formatter
+    formatter = logging.Formatter("%(name)-20s - %(levelname)-8s - %(message)s")
+    # formatter =logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s %(name)s \n- %(message)s')#,
+    console_handle.setFormatter(formatter)
+
+    # now add new handler to logger
+    logger.addHandler(console_handle)
+
+
+
+    return logger, loggername
+
+
+
+
+def print_filelist(fllst, name=None):
+    '''
+    print elements of list
+    '''
+    print('The List ({}) contains:'.format(name))
+    for item in fllst:
+        print(os.path.basename(item))#+'\n')
+    return
